c32b95b104b1ab34837746840e83aeb7b83a6220
dishant-cogni <82970851+dishant-cogni@users.noreply.github.com>
2021-06-03 04:16:37 +0530

    Finalized dashboard page

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -59,96 +59,174 @@ const Dashboard = () => {
   return (
     <>
       <Container fluid className="component-wrapper px-0 py-2">
-        <Container
-          fluid
-          className="d-flex align-items-center justify-content-start"
-        >
-          <div>
-            <BsCalendar color={primaryColor} size={24} className="mr-3" />
+        <div className="card">
+          <div className="card-content">
+            <Container
+              fluid
+              className="d-flex align-items-center justify-content-start"
+            >
+              <div>
+                <BsCalendar color={primaryColor} size={24} className="mr-3" />
+              </div>
+              <DateRangePicker
+                startDate={start_date}
+                startDateId={"start_date"}
+                endDate={end_date}
+                endDateId={"end_date"}
+                onDatesChange={({ startDate, endDate }) => {
+                  if (startDate) setStartDate(startDate);
+                  if (endDate) setEndDate(endDate);
+                }}
+                focusedInput={focusedInput}
+                onFocusChange={(focusedInput) => setFocusInput(focusedInput)}
+              />
+            </Container>
           </div>
-          <DateRangePicker
-            startDate={start_date}
-            startDateId={"start_date"}
-            endDate={end_date}
-            endDateId={"end_date"}
-            onDatesChange={({ startDate, endDate }) => {
-              if (startDate) setStartDate(startDate);
-              if (endDate) setEndDate(endDate);
-            }}
-            focusedInput={focusedInput}
-            onFocusChange={(focusedInput) => setFocusInput(focusedInput)}
-          />
+        </div>
+
+        <Container fluid className="px-0">
+          <div className="d-flex justify-content-between my-3">
+            <div className="card hoverable stats-card d-flex w-100 mr-3">
+              <div className="card-content">
+                <div className="lead">Customers</div>
+
+                <div className="d-flex align-items-center justify-content-between mt-2">
+                  <h1 className="text-black font-weight-bold">
+                    {data.overall_status.customers}
+                  </h1>
+                  <span>graph</span>
+                </div>
+
+                <div className="d-flex align-items-center justify-content-start">
+                  <span className="tag tag-success d-flex align-items-center">
+                    <AiOutlineArrowUp size={13} />
+                    <strong>10%</strong>
+                  </span>
+
+                  <span className="text-muted ml-2">from 3</span>
+                </div>
+              </div>
+            </div>
+
+            <div className="card hoverable stats-card d-flex w-100 mr-3 ml-3">
+              <div className="card-content">
+                <div className="lead">Orders</div>
+
+                <div className="d-flex align-items-center justify-content-between mt-2">
+                  <h1 className="text-black font-weight-bold">
+                    {data.overall_status.orders}
+                  </h1>
+                  <span>graph</span>
+                </div>
+
+                <div className="d-flex align-items-center justify-content-start">
+                  <span className="tag tag-success d-flex align-items-center">
+                    <AiOutlineArrowUp size={13} />
+                    <strong>10%</strong>
+                  </span>
+
+                  <span className="text-muted ml-2">from 3</span>
+                </div>
+              </div>
+            </div>
+
+            <div className="card hoverable stats-card d-flex w-100 mr-3 ml-3">
+              <div className="card-content">
+                <div className="lead">Active Subscriptions</div>
+
+                <div className="d-flex align-items-center justify-content-between mt-2">
+                  <h1 className="text-black font-weight-bold">
+                    {data.overall_status.active_subscriptions}
+                  </h1>
+                  <span>graph</span>
+                </div>
+
+                <div className="d-flex align-items-center justify-content-start">
+                  <span className="tag tag-success d-flex align-items-center">
+                    <AiOutlineArrowUp size={13} />
+                    <strong>10%</strong>
+                  </span>
+
+                  <span className="text-muted ml-2">from 3</span>
+                </div>
+              </div>
+            </div>
+
+            <div className="card hoverable stats-card d-flex w-100 ml-3">
+              <div className="card-content">
+                <div className="lead">Agents</div>
+
+                <div className="d-flex align-items-center justify-content-between mt-2">
+                  <h1 className="text-black font-weight-bold">
+                    {data.overall_status.merchants}
+                  </h1>
+                  <span>graph</span>
+                </div>
+
+                <div className="d-flex align-items-center justify-content-start">
+                  <span className="tag tag-success d-flex align-items-center">
+                    <AiOutlineArrowUp size={13} />
+                    <strong>10%</strong>
+                  </span>
+
+                  <span className="text-muted ml-2">from 3</span>
+                </div>
+              </div>
+            </div>
+          </div>
         </Container>
-        <hr />
+
         <div className="dashboard-page w-100 mt-4">
-          <Container fluid className="status-container mt-0">
-            <div className="head-row">
-              <div className="card p-2 d-flex">
-                <div className="d-flex flex-column">
-                  <div className="lead">Customers</div>
-                  <div className="d-flex align-items-end">
-                    <p className=" display-4 text-primary m-0 font-weight-bold">
-                      {data.overall_status.customers}
-                    </p>
-                    <span className="text-success d-flex  align-items-center mb-3 ml-auto">
-                      <AiOutlineArrowUp size={24} />
-                      <strong>10%</strong>
-                    </span>
-                  </div>
-                </div>
+          <Container fluid className="status-container mt-0 pl-2 pr-0">
+            <div className="card hoverable stats-card d-flex w-100">
+              <div className="card-header">
+                <p className="text-black">Reports overview</p>
               </div>
 
-              <div className="card p-2 d-flex">
-                <div className="d-flex flex-column">
-                  <div className="lead">Orders</div>
-                  <div className="d-flex align-items-end">
-                    <p className=" display-4 text-primary m-0 font-weight-bold">
-                      {data.overall_status.orders}
-                    </p>
-                    <span className="text-success d-flex  align-items-center mb-3 ml-auto">
-                      <AiOutlineArrowUp size={24} />
-                      <strong>10%</strong>
-                    </span>
-                  </div>
-                </div>
-              </div>
+              <div className="card-content">
+                <div className="d-flex align-items-center justify-content-between w-100">
+                  <span className="text-grey">Bookings</span>
 
-              <div className="card p-2 d-flex">
-                <div className="d-flex flex-column">
-                  <div className="lead">Active Subscriptions</div>
-                  <div className="d-flex align-items-end">
-                    <p className=" display-4 text-primary m-0 font-weight-bold">
-                      {data.overall_status.active_subscriptions}
-                    </p>
-                    <span className="text-success d-flex  align-items-center mb-3 ml-auto">
-                      <AiOutlineArrowUp size={24} />
-                      <strong>10%</strong>
-                    </span>
-                  </div>
-                </div>
-              </div>
+                  <span className="text-grey">100</span>
 
-              <div className="card p-2 d-flex">
-                <div className="d-flex flex-column">
-                  <div className="lead">Merchants</div>
-                  <div className="d-flex align-items-end">
-                    <p className=" display-4 text-primary m-0 font-weight-bold">
-                      {data.overall_status.merchants}
-                    </p>
-                    <span className="text-success d-flex  align-items-center mb-3 ml-auto">
-                      <AiOutlineArrowUp size={24} />
-                      <strong>10%</strong>
-                    </span>
-                  </div>
+                  <span className="tag tag-success d-flex align-items-center">
+                    <AiOutlineArrowUp size={13} />
+                    <strong>10%</strong>
+                  </span>
+                </div>
+                <hr />
+
+                <div className="d-flex align-items-center justify-content-between w-100">
+                  <span className="text-grey">Subscriptions</span>
+
+                  <span className="text-grey">100</span>
+
+                  <span className="tag tag-success d-flex align-items-center">
+                    <AiOutlineArrowUp size={13} />
+                    <strong>10%</strong>
+                  </span>
+                </div>
+                <hr />
+
+                <div className="d-flex align-items-center justify-content-between w-100">
+                  <span className="text-grey">Other</span>
+
+                  <span className="text-grey">100</span>
+
+                  <span className="tag tag-success d-flex align-items-center">
+                    <AiOutlineArrowUp size={13} />
+                    <strong>10%</strong>
+                  </span>
                 </div>
               </div>
             </div>
           </Container>
-          <Container fluid className="charts-container mt-2">
+
+          <Container fluid className="charts-container mt-0 pl-0 pr-2">
             <div className="charts-row">
-              <div className="card ">
-                <div className="chart-setting px-1 py-2 d-flex justify-content-between align-items-center">
-                  <h5 className="">
+              <div className="card">
+                <div className="card-header pb-3 d-flex justify-content-between align-items-center">
+                  <h5 className="mb-0">
                     <strong>Bookings</strong>
                   </h5>
                   <div>
@@ -164,20 +242,22 @@ const Dashboard = () => {
                     </Form.Control>
                   </div>
                 </div>
-                <div className="chart-container">
+
+                <div className="card-content chart-container">
                   <ChartLine />
                 </div>
               </div>
+
               <div className="card ">
-                <div className="chart-setting p-1 d-flex justify-content-between">
-                  <h5 className="">
-                    <strong>Chart Title</strong>
+                <div className="card-header pb-3 d-flex justify-content-between align-items-center">
+                  <h5 className="mb-0">
+                    <strong>Bookings</strong>
                   </h5>
                   <div>
                     <Form.Control
                       as="select"
                       custom
-                      onChange={handleChartTwoChange}
+                      onChange={handleChartOneChange}
                       className="bg-transparent"
                     >
                       <option value="1">Option 1</option>
@@ -186,20 +266,22 @@ const Dashboard = () => {
                     </Form.Control>
                   </div>
                 </div>
-                <div className=" chart-container">
+
+                <div className="card-content chart-container">
                   <ChartArea />
                 </div>
               </div>
+
               <div className="card ">
-                <div className="chart-setting p-1 d-flex justify-content-between">
-                  <h5 className="">
-                    <strong>Chart Title</strong>
+                <div className="card-header pb-3 d-flex justify-content-between align-items-center">
+                  <h5 className="mb-0">
+                    <strong>Bookings</strong>
                   </h5>
                   <div>
                     <Form.Control
                       as="select"
                       custom
-                      onChange={handleChartTwoChange}
+                      onChange={handleChartOneChange}
                       className="bg-transparent"
                     >
                       <option value="1">Option 1</option>
@@ -208,30 +290,9 @@ const Dashboard = () => {
                     </Form.Control>
                   </div>
                 </div>
-                <div className=" chart-container">
+
+                <div className="card-content chart-container">
                   <ChartBar />
-                </div>
-              </div>
-              <div className="card ">
-                <div className="chart-setting p-1 d-flex justify-content-between">
-                  <h5 className="">
-                    <strong>Chart Title</strong>
-                  </h5>
-                  <div>
-                    <Form.Control
-                      as="select"
-                      custom
-                      onChange={handleChartTwoChange}
-                      className="bg-transparent"
-                    >
-                      <option value="1">Option 1</option>
-                      <option value="2">Option 2</option>
-                      <option value="3">Option 3</option>
-                    </Form.Control>
-                  </div>
-                </div>
-                <div className=" chart-container">
-                  <ChartArea />
                 </div>
               </div>
             </div>
--- src/shared-components/Logo.tsx
+++ src/shared-components/Logo.tsx
@@ -1,8 +1,8 @@
 import logo from "../assets/logo.jpg";
 const Logo = () => {
   return (
-    <div className="d-flex justify-content-center">
-      <div className="logo text-center">
+    <div className="d-flex justify-content-start">
+      <div className="logo text-left ml-4">
         <img src={logo} alt="logo" />
         {/* <h2 style={{ color }}>Carwash</h2> */}
       </div>
