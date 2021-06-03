18c92796f0e0557743c5a10bb45343166e66c771
unknown <webdev.hitesh@gmail.com>
2021-06-03 20:26:54 +0530

    user create update bug fix

--- src/components/Users/UsersCreateUpdateForm.tsx
+++ src/components/Users/UsersCreateUpdateForm.tsx
@@ -1,15 +1,23 @@
+import { useLocation } from "react-router-dom";
 import UserAddress from "./UserAddresses";
 import UserBasics from "./UserBasics";
 import UserVehicles from "./UserVechiles";
 
 
 const UserCreateUpdateForm = () => {
-
+  const { state } = useLocation()
+  const id = state ? (state as any).id : null;
+  console.log({ id })
   return (
     <>
       <UserBasics />
-      <UserAddress />
-      <UserVehicles />
+      {
+        id &&
+        <>
+          <UserAddress />
+          <UserVehicles />
+        </>
+      }
     </>
   );
 };
