8530683bd797052806b2403bfde4a5a4507b0610
Dishant Agnihotri <dishantagnihotri@gmail.com>
2025-07-30 13:59:22 +0530

    Update sidebar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -18,7 +18,6 @@ import { GiModernCity, GiOnTarget } from "react-icons/gi";
 import { FaUserCog } from "react-icons/fa";
 import { BiLogOut } from "react-icons/bi";
 
-
 import {
   RiAdminFill,
   RiAdvertisementFill,
@@ -42,7 +41,6 @@ import { SiCivicrm } from "react-icons/si";
 import { GrOrganization } from "react-icons/gr";
 import profile from "../assets/profile.svg";
 
-
 import { GrDocumentConfig } from "react-icons/gr";
 import { primaryColor } from "../utils/constants";
 import useUserProfileStore from "../hooks/useUserProfileStore";
@@ -51,7 +49,97 @@ import { useMutation } from "react-query";
 import useTokenStore from "../hooks/useTokenStore";
 import API from "../utils/API";
 
-const manageLinks: Array<INavLink> = [
+const mainLinks: Array<INavLink> = [
+  {
+    title: "Orders",
+    path: "/orders",
+    icon: <FaBoxes />,
+    permissionReq: "read_booking",
+  },
+  {
+    title: "CRM",
+    path: "/crm",
+    icon: <SiCivicrm />,
+    permissionReq: "read_city",
+  },
+  {
+    title: "Bookings",
+    path: "/crm-bookings",
+    icon: <BsClock />,
+    permissionReq: "read_bookingslot",
+  },
+  {
+    title: "Cart",
+    path: "/cart",
+    icon: <MdShoppingCart />,
+    permissionReq: "read_booking",
+  },
+  {
+    title: "Invoices",
+    path: "/Invoices",
+    icon: <SiCivicrm />,
+    permissionReq: "read_city",
+  },
+  {
+    title: "Customers",
+    path: "/users",
+    icon: <ImUsers />,
+    permissionReq: "read_user",
+  },
+  {
+    title: "Subscriptions",
+    path: "/subscriptions",
+    icon: <FaAddressCard />,
+    permissionReq: "read_subscription",
+  },
+  {
+    title: "Transactions",
+    path: "/transactions",
+    icon: <FaMoneyCheck />,
+    permissionReq: "read_transaction",
+  },
+  {
+    title: "Help Center",
+    path: "/issues",
+    icon: <GoIssueOpened />,
+    permissionReq: "read_ticket",
+  },
+];
+
+const websiteLinks: Array<INavLink> = [
+  {
+    title: "Faqs",
+    path: "/faqs",
+    icon: <FaQuestionCircle />,
+    permissionReq: "read_faq",
+  },
+  {
+    title: "Coupons",
+    path: "/coupons",
+    icon: <RiCoupon3Line />,
+    permissionReq: "read_coupon",
+  },
+  {
+    title: "Banners",
+    path: "/advertisements",
+    icon: <RiAdvertisementFill />,
+    permissionReq: "read_banner",
+  },
+  {
+    title: "Testimonials",
+    path: "/testimonials",
+    icon: <AiFillIdcard />,
+    permissionReq: "read_testimonial",
+  },
+  {
+    title: "Static Pages",
+    path: "/static-pages",
+    icon: <RiPagesLine />,
+    permissionReq: "read_staticpage",
+  },
+];
+
+const inventoryLinks: Array<INavLink> = [
   {
     title: "Products",
     path: "/products",
@@ -77,17 +165,20 @@ const manageLinks: Array<INavLink> = [
     permissionReq: "read_category",
   },
   {
-    title: "Invoices",
-    path: "/Invoices",
-    icon: <SiCivicrm />,
-    permissionReq: "read_city",
+    title: "Services",
+    path: "/services",
+    icon: <RiServiceFill />,
+    permissionReq: "read_service",
   },
   {
-    title: "Customers",
-    path: "/users",
-    icon: <ImUsers />,
-    permissionReq: "read_user",
+    title: "Plans",
+    path: "/plans",
+    icon: <FaRegLightbulb />,
+    permissionReq: "read_plan",
   },
+];
+
+const organisationLinks: Array<INavLink> = [
   {
     title: "Team Members",
     path: "/team-members",
@@ -107,71 +198,18 @@ const manageLinks: Array<INavLink> = [
     permissionReq: "read_agenttarget",
   },
   {
-    title: "Services",
-    path: "/services",
-    icon: <RiServiceFill />,
-    permissionReq: "read_service",
-  },
-  {
-    title: "Faqs",
-    path: "/faqs",
-    icon: <FaQuestionCircle />,
-    permissionReq: "read_faq",
-  },
-  {
-    title: "Plans",
-    path: "/plans",
-    icon: <FaRegLightbulb />,
-    permissionReq: "read_plan",
-  },
-  {
-    title: "Coupons",
-    path: "/coupons",
-    icon: <RiCoupon3Line />,
-    permissionReq: "read_coupon",
-  },
-  {
-    title: "Banners",
-    path: "/advertisements",
-    icon: <RiAdvertisementFill />,
-    permissionReq: "read_banner",
-  },
-  // {
-  //   title: "CMS",
-  //   path: "/cms",
-  //   icon: <FaQuestionCircle />,
-  //   permissionReq: "read_faq",
-  // },
-  {
-    title: "Testimonials",
-    path: "/testimonials",
-    icon: <AiFillIdcard />,
-    permissionReq: "read_testimonial",
-  },
-  {
-    title: "Static Pages",
-    path: "/static-pages",
-    icon: <RiPagesLine />,
-    permissionReq: "read_staticpage",
-  },
-  {
     title: "Roles & Permissions",
     path: "/permissions",
     icon: <BsShieldLock />,
     permissionReq: "read_permission",
   },
-  // {
-  //   title: "Roles",
-  //   path: "/roles",
-  //   icon: <FaUsers />,
-  //   permissionReq: "read_role",
-  // },
   {
     title: "Configurations",
     path: "/configurations",
     icon: <AiFillSetting />,
     permissionReq: "read_config",
   },
+
   {
     title: "Organization",
     path: "/organization",
@@ -184,58 +222,6 @@ const manageLinks: Array<INavLink> = [
     icon: <GiModernCity />,
     permissionReq: "read_city",
   },
-
-];
-
-const mainLinks: Array<INavLink> = [
-  {
-    title: "Orders",
-    path: "/orders",
-    icon: <FaBoxes />,
-    permissionReq: "read_booking",
-  },
-  // {
-  //   title: "CRM",
-  //   path: "/crm",
-  //   icon: <FaUsersCog />,
-  //   permissionReq: "read_booking",
-  // },
-  {
-    title: "CRM",
-    path: "/crm",
-    icon: <SiCivicrm />,
-    permissionReq: "read_city",
-  },
-  {
-    title: "CRM Bookings",
-    path: "/crm-bookings",
-    icon: <BsClock />,
-    permissionReq: "read_bookingslot",
-  },
-  {
-    title: "Cart",
-    path: "/cart",
-    icon: <MdShoppingCart />,
-    permissionReq: "read_booking",
-  },
-  {
-    title: "Subscriptions",
-    path: "/subscriptions",
-    icon: <FaAddressCard />,
-    permissionReq: "read_subscription",
-  },
-  {
-    title: "Transactions",
-    path: "/transactions",
-    icon: <FaMoneyCheck />,
-    permissionReq: "read_transaction",
-  },
-  {
-    title: "Help Center",
-    path: "/issues",
-    icon: <GoIssueOpened />,
-    permissionReq: "read_ticket",
-  },
   {
     title: "Notification/Sms",
     path: "/push-notifications",
@@ -244,8 +230,24 @@ const mainLinks: Array<INavLink> = [
   },
 ];
 
-const hiddenRoutesForCRM = ["/orders", "/cart", "/plans", "/coupons", "/agent", "/agent-targets", "/cities", "/product-variants"];
-const hiddenRoutesForEcommerce = ["/crm", "/crm-bookings", "/services", "/products", "/product-brands", "/categories"];
+const hiddenRoutesForCRM = [
+  "/orders",
+  "/cart",
+  "/plans",
+  "/coupons",
+  "/agent",
+  "/agent-targets",
+  "/cities",
+  "/product-variants",
+];
+const hiddenRoutesForEcommerce = [
+  "/crm",
+  "/crm-bookings",
+  "/services",
+  "/products",
+  "/product-brands",
+  "/categories",
+];
 const logout = () => {
   return API.post("/auth/logout");
 };
@@ -268,24 +270,44 @@ const NavBar = ({ isNavOpen, setIsNavOpen }: INavBar) => {
       setIsNavOpen(!isNavOpen);
     }
   };
+  console.log("loggedInUser", loggedInUser);
+  if (
+    loggedInUser.organisation &&
+    !loggedInUser.organisation.hasOwnProperty("store_type")
+  )
+    loggedInUser.organisation.store_type = "crm"; // setting default to CRM if no organisation found
 
-  if (loggedInUser.organisation && !loggedInUser.organisation.hasOwnProperty("store_type")) loggedInUser.organisation.store_type = "crm"; // setting default to CRM if no organisation found
-
-  console.log('loggedInUser', loggedInUser)
+  console.log("loggedInUser", loggedInUser);
 
   // Filtered links for sidebar
-  const filteredMainLinks = mainLinks.filter(link => {
-    if (loggedInUser?.organisation.store_type.toLowerCase() === 'ecommerce') {
+  const filteredMainLinks = mainLinks.filter((link) => {
+    if (loggedInUser?.organisation.store_type.toLowerCase() === "ecommerce") {
       return hiddenRoutesForEcommerce.includes(link.path) ? false : true;
-    } else if (loggedInUser?.organisation.store_type.toLowerCase() === 'crm') {
+    } else if (loggedInUser?.organisation.store_type.toLowerCase() === "crm") {
       return hiddenRoutesForCRM.includes(link.path) ? false : true;
     }
   });
 
-  const filteredManageLinks = manageLinks.filter(link => {
-    if (loggedInUser?.organisation.store_type.toLowerCase() === 'ecommerce') {
+  const filteredWebsiteinks = websiteLinks.filter((link) => {
+    if (loggedInUser?.organisation.store_type.toLowerCase() === "ecommerce") {
       return hiddenRoutesForEcommerce.includes(link.path) ? false : true;
-    } else if (loggedInUser?.organisation.store_type.toLowerCase() === 'crm') {
+    } else if (loggedInUser?.organisation.store_type.toLowerCase() === "crm") {
+      return hiddenRoutesForCRM.includes(link.path) ? false : true;
+    }
+  });
+
+  const filterinventoryLinks = inventoryLinks.filter((link) => {
+    if (loggedInUser?.organisation.store_type.toLowerCase() === "ecommerce") {
+      return hiddenRoutesForEcommerce.includes(link.path) ? false : true;
+    } else if (loggedInUser?.organisation.store_type.toLowerCase() === "crm") {
+      return hiddenRoutesForCRM.includes(link.path) ? false : true;
+    }
+  });
+
+  const filterOrganisationLinks = organisationLinks.filter((link) => {
+    if (loggedInUser?.organisation.store_type.toLowerCase() === "ecommerce") {
+      return hiddenRoutesForEcommerce.includes(link.path) ? false : true;
+    } else if (loggedInUser?.organisation.store_type.toLowerCase() === "crm") {
       return hiddenRoutesForCRM.includes(link.path) ? false : true;
     }
   });
@@ -309,18 +331,27 @@ const NavBar = ({ isNavOpen, setIsNavOpen }: INavBar) => {
         {isDesktop && (
           <div className="d-flex  justify-content-between align-items-center">
             <Logo />
-            {/* <FaArrowLeft
-              onClick={desktopNavClose}
-              // color={""}
-              size={20}
-              className="mr-2"
-              style={{ cursor: "pointer", color: "#707070" }}
-            /> */}
             <div className="collapse-svg">
               <svg
                 className="mr-3 w-20 bi bi-arrow-bar-left"
-                style={{ cursor: "pointer", color: "#181d27", width: "25px", height: "25px", marginRight: "5px" }} onClick={desktopNavClose} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-                <path fillRule="evenodd" d="M12.5 15a.5.5 0 0 1-.5-.5v-13a.5.5 0 0 1 1 0v13a.5.5 0 0 1-.5.5M10 8a.5.5 0 0 1-.5.5H3.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L3.707 7.5H9.5a.5.5 0 0 1 .5.5" />
+                style={{
+                  cursor: "pointer",
+                  color: "#181d27",
+                  width: "25px",
+                  height: "25px",
+                  marginRight: "5px",
+                }}
+                onClick={desktopNavClose}
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M12.5 15a.5.5 0 0 1-.5-.5v-13a.5.5 0 0 1 1 0v13a.5.5 0 0 1-.5.5M10 8a.5.5 0 0 1-.5.5H3.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L3.707 7.5H9.5a.5.5 0 0 1 .5.5"
+                />
               </svg>
             </div>
           </div>
@@ -337,8 +368,7 @@ const NavBar = ({ isNavOpen, setIsNavOpen }: INavBar) => {
               permissionReq="read_dashboard"
             />
           </ul>
-
-          <p className="text-muted mb-2">Main</p>
+          <p className="text-muted mb-2">Leads & Customers</p>
           <ul>
             {filteredMainLinks.map(({ title, path, icon, permissionReq }) => (
               <Navlink
@@ -352,10 +382,25 @@ const NavBar = ({ isNavOpen, setIsNavOpen }: INavBar) => {
               />
             ))}
           </ul>
-
-          <p className="text-muted mb-2">Manage</p>
+          <p className="text-muted mb-2">Products & Services</p>
           <ul>
-            {filteredManageLinks.map(({ title, path, icon, permissionReq }) => (
+            {filterinventoryLinks.map(
+              ({ title, path, icon, permissionReq }) => (
+                <Navlink
+                  path={path}
+                  title={title}
+                  key={title}
+                  onClick={closeNavBar}
+                  icon={icon}
+                  isNavOpen={isNavOpen}
+                  permissionReq={permissionReq}
+                />
+              )
+            )}
+          </ul>
+          <p className="text-muted mb-2">Website & Pages</p>
+          <ul>
+            {filteredWebsiteinks.map(({ title, path, icon, permissionReq }) => (
               <Navlink
                 path={path}
                 title={title}
@@ -367,9 +412,43 @@ const NavBar = ({ isNavOpen, setIsNavOpen }: INavBar) => {
               />
             ))}
           </ul>
+          <p className="text-muted mb-2">Organization & Settings</p>
+          <ul>
+            {filterOrganisationLinks.map(
+              ({ title, path, icon, permissionReq }) => (
+                <Navlink
+                  path={path}
+                  title={title}
+                  key={title}
+                  onClick={closeNavBar}
+                  icon={icon}
+                  isNavOpen={isNavOpen}
+                  permissionReq={permissionReq}
+                />
+              )
+            )}
+          </ul>
         </div>
-        <div className="top-bar d-flex align-items-center user-dd" style={{ borderTop: '1px solid #E0E0E0', borderBottom: '0', bottom: '0', top: 'unset', padding: '0 20px' }}>
-          <Dropdown className="global-card w-100" style={{ background: '#fff', borderRadius: '10px', boxShadow: '0px 15px 32px 0px #0000000D, 0px 59px 59px 0px #0000000A, 0px 132px 79px 0px #00000008, 0px 234px 94px 0px #00000003, 0px 366px 103px 0px #00000000', padding: '12px 20px' }}>
+        <div
+          className="top-bar d-flex align-items-center user-dd"
+          style={{
+            borderTop: "1px solid #E0E0E0",
+            borderBottom: "0",
+            bottom: "0",
+            top: "unset",
+            padding: "0 20px",
+          }}
+        >
+          <Dropdown
+            className="global-card w-100"
+            style={{
+              background: "#fff",
+              borderRadius: "10px",
+              boxShadow:
+                "0px 15px 32px 0px #0000000D, 0px 59px 59px 0px #0000000A, 0px 132px 79px 0px #00000008, 0px 234px 94px 0px #00000003, 0px 366px 103px 0px #00000000",
+              padding: "12px 20px",
+            }}
+          >
             <Dropdown.Toggle
               id="dropdown-basic"
               className="d-flex align-items-center filter-button bg-transparent border-0 p-0"
@@ -384,13 +463,17 @@ const NavBar = ({ isNavOpen, setIsNavOpen }: INavBar) => {
             </Dropdown.Toggle>
 
             <Dropdown.Menu className="global-card">
-              <Dropdown.Item href="/profile" className="border-bottom "> <FaUserCog className="mr-3" />Profile</Dropdown.Item>
+              <Dropdown.Item href="/profile" className="border-bottom ">
+                {" "}
+                <FaUserCog className="mr-3" />
+                Profile
+              </Dropdown.Item>
               <Dropdown.Item onClick={() => mutate()}>
-                <BiLogOut className="mr-3" /> {isLoading ? "Loading" : "Log out"}
+                <BiLogOut className="mr-3" />{" "}
+                {isLoading ? "Loading" : "Log out"}
               </Dropdown.Item>
             </Dropdown.Menu>
           </Dropdown>
-
         </div>
       </nav>
       {isNavOpen && !isDesktop && <Overlay onClick={closeNavBar} />}
