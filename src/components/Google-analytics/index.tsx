9adf762c4cbdc1119b387333b66f3211bbaee4ba
ajayk8055 <ajay@deliverable.services>
2025-08-04 16:24:19 +0530

    bug fix crm and new page added google analytics

---
+++ src/components/Google-analytics/index.tsx
@@ -0,0 +1,13 @@
+// google-analytics.tsx or main layout file
+import React from "react";
+
+
+const GoogleAnalytics = () => {
+    return (
+        <>
+           
+        </>
+    );
+};
+
+export default GoogleAnalytics;
